8e45147d18fa5566804d63ba3a5a4c762c1f3aa4
Ricky Chien <ricky060709@gmail.com>
2013-10-06 16:30:34 +0800

    Completed gas.getPrice function

--- routes/gas.js
+++ routes/gas.js
@@ -3,6 +3,37 @@
  * GET gas information.
  */
 
-exports.getPrice = function(req, res){
-  res.send("price");
+var http = require("http");
+var options = {
+  host: 'gas.goodlife.tw',
+  path: '/gas.json'
+};
+
+var getJSON = function(options, onResult) {
+  var req = http.request(options, function(res) {
+    var output = '';
+    console.log(options.host + ':' + res.statusCode);
+    res.setEncoding('utf8');
+
+    res.on('data', function (chunk) {
+      output += chunk;
+    });
+
+    res.on('end', function() {
+      var data = JSON.parse(output);
+      onResult(res.statusCode, data);
+    });
+  });
+
+  req.on('error', function(err) {
+    res.send('error: ' + err.message);
+  });
+
+  req.end();
+};
+
+exports.getPrice = function(req, res) {
+  getJSON(options, function(statusCode, data) {
+    res.send(data);
+  });
 };
